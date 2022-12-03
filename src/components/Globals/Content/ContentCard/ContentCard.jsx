13a6bc2fe30e2b3fa7005020e0dc503aab7fbcb5
Felix Adam <felix.adam@tx.group>
2022-12-03 16:12:30 +0100

    Pizza-content: font config card basics

--- src/components/Globals/Content/ContentCard/ContentCard.jsx
+++ src/components/Globals/Content/ContentCard/ContentCard.jsx
@@ -6,19 +6,30 @@ import "./ContentCard.css"
 
 
 export const ContentCard = (props) => {
-  console.log('contentCard props',props)
+  const cardStyle = [
+  "flex flex-start justify-center items-start bg-white py-8 px-12 rounded-full "
+  ]
+
+  console.log('contentCard style')
   return (
-    <article className='root'>
+    <article className={['root', ...cardStyle].join(' ')}>
       <div>
-        <h1>{props.text}</h1>
+        <h1 className='title'>{props.text}</h1>
           <span>
-            <span>Username</span>
+            <span>Username </span> |   
             <span>Timestamp</span>
           </span>
-          <p>Text... .lots of text. lots of text. lots of text.</p>
-          <figure>
-            <span>here is the image</span>
+        <div>
+          <p className='message'>Paragraph: Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+        </div>  
+          <figure className='image' >
+            <span>here is the image component</span>
           </figure>
+        <div className='socialInteraction'>
+          <span>Comment-icon</span> |
+          <span>Like-icon</span> |
+          <span>CopyLink-icon</span>
+        </div>
       </div>
     </article>
     )
