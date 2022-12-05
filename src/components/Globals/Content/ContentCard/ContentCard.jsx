2be2a2708379f40a95a909d90b53015e7c758aca
Felix Adam <felix.adam@tx.group>
2022-12-05 19:57:10 +0100

    pizza-content: contentCard adaptions

--- src/components/Globals/Content/ContentCard/ContentCard.jsx
+++ src/components/Globals/Content/ContentCard/ContentCard.jsx
@@ -16,20 +16,21 @@ export const ContentCard = (props) => {
   const { size } = props 
 
   const sizeStyle = size === 's' ? '-variantSmall' : '-variantLarge'
+  const textSize = [`M-text-size-${size}`]
 
   return (
     <article className={['contentCard', ...cardStyle].join(' ')}>
       <div>
         <div className={sizeStyle} >
           <UserProfile size='m' />
-          <h1 className='title'>{props.text}</h1>
+          <h1 className={['title', ...textSize].join(' ')}>{props.text}</h1>
         </div>
             <span className='metainfo'>
              <UserName username={'Display Name'} />
               <TimeStamp time={'13:32'} />
             </span>
         <div>
-          <p className={['sm:text-sm, base:text-base message']}>Paragraph: Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+          <p className={['message', ...textSize].join(' ')}>Paragraph: Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
         </div>  
           <figure className='image' >
             <span>here is the image component</span>
