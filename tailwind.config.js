ab819f88eee6fd5c0c9f6fd1c73d81130ea22303
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2022-12-11 23:50:52 +0100

    feat: remove test

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,29 +8,7 @@ module.exports = {
 		fontFamily: {
 			sans: ["Poppins", ...defaultTheme.fontFamily.sans],
 		},
-		fontSize: {
-			xs: "0.875rem",
-			sm: "1rem",
-			base: "1.125rem",
-			lg: "1.25rem",
-			"2xl": "1.5rem",
-			"3xl": "2rem",
-			"4xl": "2.5rem",
-			"5xl": "3rem",
-		},
-		fontWeight: {
-			medium: "500",
-			semibold: "600",
-			bold: "700",
-		},
-		lineHeight: {
-			none: "1",
-			tight: "1.25",
-			snug: "1.4",
-			normal: "1.45",
-			relaxed: "1.625",
-			loose: "2",
-		},
+
 		extend: {
 			colors: {
 				transparent: "transparent",
