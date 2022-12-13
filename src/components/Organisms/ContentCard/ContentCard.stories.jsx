6452311e0bc326ad043f656e6f75d97a3c20fbe4
Felix Adam <felix.adam@tx.group>
2022-12-13 19:03:28 +0100

    feat: card variant responsive

--- src/components/Organisms/ContentCard/ContentCard.stories.jsx
+++ src/components/Organisms/ContentCard/ContentCard.stories.jsx
@@ -15,16 +15,18 @@ const Template = (args) => <ContentCard {...args} />
 
 export const Detailpage = Template.bind()
 Detailpage.args = {
-	text: "Hi I am a Large template",
+	text: "Hi I am the Detail template - Big and Bold",
 	variant: "detailpage",
 };
 
 export const Timeline = Template.bind()
 Timeline.args = {
-	text: "Medium- rare, mostly we well use that one",
+	text: "Medium - rare, mostly we well use that one",
 	variant: "timeline",
 };
 
-
-// export const Text = () => Template.bind({});
-
+export const Responsive = Template.bind()
+Responsive.args = {
+	text: "Responsive - all mobiles see me that way",
+	variant: "responsive",
+};
