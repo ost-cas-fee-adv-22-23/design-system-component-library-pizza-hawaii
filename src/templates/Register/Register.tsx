b79e77142d8cfed3eb15634c3841c8d2c91733dd
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2022-12-13 22:13:32 +0100

    feat: add register page

--- src/templates/Register/Register.tsx
+++ src/templates/Register/Register.tsx
@@ -14,12 +14,14 @@ export const Register: FC<BaseProps> = () => {
 		<SplitScreen>
 			<Headline level="2">Anmelden</Headline>
 			<Form cta="Let’s mumble">
+				<FormInput label="Vorname Name" type="text" />
+				<FormInput label="User Name" type="text" />
 				<FormInput label="E-Mail" type="email" />
 				<FormInput label="Passwort" type="password" />
 			</Form>
 			<div className="mt-3 text-center">
 				<Label>
-					Noch kein Account? <Link href="/">Jetzt registrieren</Link>
+					Bereits registriert? <Link href="/">Jetzt anmelden</Link>
 				</Label>
 			</div>
 		</SplitScreen>
