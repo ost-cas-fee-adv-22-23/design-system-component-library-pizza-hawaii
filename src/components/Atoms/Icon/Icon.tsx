40dc2317a9f1a1fdd3d9314599ec486796a7ff8e
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-01-08 23:20:49 +0100

    feat: improve code by reuse svg’s

--- src/components/Atoms/Icon/Icon.tsx
+++ src/components/Atoms/Icon/Icon.tsx
@@ -13,14 +13,34 @@ const sizeStyle = {
 	L: ['w-[28px]'],
 };
 
+const counter: Record<string, boolean> = IcoNames.reduce((acc, name) => ({ ...acc, [name]: false }), {});
+
 export const Icon: FC<IconProps> = ({ size = 'M', name = IcoNames[0], ...props }) => {
 	const style = [...(sizeStyle[size] || sizeStyle.M)];
+
+	let svgIcon = IconLib[name];
+
+	if (!counter[name]) {
+		const svgCode = svgIcon.replace(`<svg`, `<svg id="${name}"`);
+
+		let p = `svg-container-${name}` && document.getElementById(`svg-container-${name}`);
+		if (!p) {
+			p = document.createElement('div');
+			p.id = `svg-container-${name}`;
+			p.innerHTML = svgCode;
+			p.style.display = 'none';
+			document.body.appendChild(p);
+		}
+		counter[name] = true;
+	}
+	svgIcon = svgIcon.replace(/<svg([^>]*)>([\s\S]*?)<\/svg>/gi, `<svg$1><use xlink:href="#${name}"></use></svg>`);
+
 
 	return (
 		<span
 			className={['Icon', 'inline-flex', ...style].join(' ')}
 			{...props}
-			dangerouslySetInnerHTML={{ __html: IconLib[name] }}
+			dangerouslySetInnerHTML={{ __html: svgIcon }}
 		></span>
 	);
 };
