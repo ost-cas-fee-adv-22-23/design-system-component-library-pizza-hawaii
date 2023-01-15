0230a5932af27a7e7de6ce7a3461de7ede2a684a
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-01-15 11:18:34 +0100

    feat: improve switch for accessibility and export

--- src/components/Molecules/Switch/Switch.tsx
+++ src/components/Molecules/Switch/Switch.tsx
@@ -19,7 +19,7 @@ type TSwitch = {
  */
 
 const fieldStyles = 'flex overflow-hidden bg-slate-200 rounded-lg p-1 w-fit';
-const inputStyles = ' absolute overflow-hidden h-0 w-0 hidden peer';
+const inputStyles = ' absolute overflow-hidden h-0 w-0 peer';
 const labelStyles =
 	'block hover:cursor-pointer hover:text-slate-800 bg-slate-200 first-of-type:rounded-tl-md first-of-type:rounded-bl-md last:rounded-tr-md last:rounded-br-md text-slate-600 text-base font-semibold text-center py-2 px-4 border-slate-600 transition-all ease-in duration-150 peer-checked:bg-white peer-checked:text-violet-600';
 
@@ -29,7 +29,7 @@ export const Switch: FC<TSwitch> = ({ options, value, name = uid('Switch'), onCh
 	const changeValue = (event: ChangeEvent): void => {
 		const target = event.target as HTMLInputElement;
 		setActiveValue(target.value);
-		onChange(target.value);
+		onChange && onChange(target.value);
 	};
 
 	return (
