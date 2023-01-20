9215da7f8e7e487994a63f102da6e4e618893a63
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-01-20 09:35:14 +0100

    fix: button style (remove commas from classes)

--- src/components/Molecules/Button/Button.tsx
+++ src/components/Molecules/Button/Button.tsx
@@ -28,7 +28,12 @@ type TButtonProps = THTMLButtonProps | TLinkButtonProps | TSpanButtonProps;
  * Styles
  */
 
-export const ButtonBaseStyle = ['flex items-center justify-center', 'w-full', 'transition-all duration-300'];
+export const ButtonBaseStyle = [
+	'flex items-center justify-center',
+	'w-full',
+	'transition-all duration-300',
+	'outline-none outline-offset-0',
+];
 
 export const ButtonSizeMap: Record<string, string> = {
 	S: 'p-2 rounded-lg gap-y-0 gap-x-2 text-sm font-semibold',
@@ -37,10 +42,24 @@ export const ButtonSizeMap: Record<string, string> = {
 };
 
 export const ButtonColorMap: Record<string, string> = {
-	slate: 'text-white bg-slate-600 outline-none outline-offset-0 hover:bg-slate-700 hover:outline-slate-100, hover:outline-3  active:outline-4, active:outline-slate-200 focus:bg-slate-700 focus:outline-slate-200 focus:outline-4',
-	violet: 'text-white bg-violet-600 outline-none outline-offset-0 hover:bg-violet-700 hover:outline-3 hover:outline-violet-100 active:outline-4, active:outline-violet-100 focus:bg-violet-700 focus:outline-violet-200 focus:outline-4',
-	gradient:
-		'text-white bg-gradient-to-r from-violet-600 via-pink-500 to-violet-700 bg-pos-0 bg-size-200 outline-none outline-offset-0 hover:bg-pos-100 hover:bg-size-300 hover:outline-3 hover:outline-violet-100 active:outline-4, active:outline-violet-200 focus:bg-pos-100 focus:bg-size-300 focus:outline-violet-200 focus:outline-4',
+	slate: [
+		'text-white bg-slate-600',
+		'hover:bg-slate-700 hover:outline-slate-100 hover:outline-3',
+		'focus:bg-slate-700 focus:outline-slate-200 focus:outline-4',
+		'active:bg-slate-700 active:outline-slate-200 active:outline-4',
+	].join(' '),
+	violet: [
+		'text-white bg-violet-600',
+		'hover:bg-violet-700 hover:outline-violet-100 hover:outline-3',
+		'focus:bg-violet-700 focus:outline-violet-200 focus:outline-4',
+		'active:bg-violet-700 active:outline-violet-200 active:outline-4',
+	].join(' '),
+	gradient: [
+		'text-white bg-gradient-to-r from-violet-600 via-pink-500 to-violet-700 bg-pos-0 bg-size-200',
+		'hover:bg-pos-100 hover:bg-size-300 hover:outline-violet-100 hover:outline-3',
+		'focus:bg-pos-100 focus:bg-size-300 focus:outline-violet-200 focus:outline-4',
+		'active:bg-pos-100 active:bg-size-300 active:outline-violet-200 active:outline-4',
+	].join(' '),
 };
 
 /*
