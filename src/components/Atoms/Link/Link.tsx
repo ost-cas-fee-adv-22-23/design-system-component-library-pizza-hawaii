a715ce974d0a1daab4e2a30b3e67a04f95133eba
Felix Adam <felix.adam@tx.group>
2023-01-21 15:03:37 +0100

    docs: documentation and Types for Link, Richtext

--- src/components/Atoms/Link/Link.tsx
+++ src/components/Atoms/Link/Link.tsx
@@ -1,19 +1,37 @@
 import React, { FC, ReactNode, AnchorHTMLAttributes, HTMLAttributes } from 'react';
 
+/**
+ * Settings
+ */
+
+export const possibleLinkTags = ['a', 'span'] as const;
+
 /*
  * Type
  */
 
-type TLink = {
-	as?: 'a' | 'span';
+export type TLink = {
+	/**
+	 * Choose between the HTML tags 'a' or 'span'
+	 * @type TLink
+	 * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/link
+	 */
+
+	as?: TLinkTag;
+	/**
+	 * provide a link reference (target url) as string
+	 */
 	href: string;
+	/**
+	 * Child Nodes
+	 */
 	children: ReactNode;
 };
 
 type THTMLLinkProps = TLink & { as: 'a' } & AnchorHTMLAttributes<HTMLAnchorElement>;
 type TSpanLinkProps = TLink & { as: 'span' } & HTMLAttributes<HTMLSpanElement>;
-
 type TLinkProps = THTMLLinkProps | TSpanLinkProps;
+type TLinkTag = typeof possibleLinkTags[number];
 
 /*
  * Style
