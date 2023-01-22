f39578fae4860627b54a264a40effa1eaeefbc75
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-01-22 21:55:48 +0100

    chore: define client components

--- src/components/Atoms/Richtext/Richtext.tsx
+++ src/components/Atoms/Richtext/Richtext.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { FC, ReactNode, Children } from 'react';
 import dompurify from 'dompurify';
 
--- src/components/Organisms/Modal/Modal.tsx
+++ src/components/Organisms/Modal/Modal.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { FC, ReactNode, useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
