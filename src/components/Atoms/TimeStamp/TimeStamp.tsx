6c213a43eef82bbb622f48df27c78cded4765d9f
Jürgen Rudigier <juergen.rudigier@deptagency.com>
2023-02-11 15:00:34 +0100

    feat: remove 'use client';

--- src/components/Atoms/TimeStamp/TimeStamp.tsx
+++ src/components/Atoms/TimeStamp/TimeStamp.tsx
@@ -1,4 +1,3 @@
-'use client';
 import React, { FC, useState, useEffect } from 'react';
 import moment from 'moment';
 
--- src/components/Organisms/Modal/Modal.tsx
+++ src/components/Organisms/Modal/Modal.tsx
@@ -1,4 +1,3 @@
-'use client';
 import React, { FC, ReactNode, useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
